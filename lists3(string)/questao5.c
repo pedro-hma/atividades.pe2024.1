83b4b63a6055fc6125cefb616819c069a9c322f8
pedro-hma <105680915+pedro-hma@users.noreply.github.com>
2024-04-25 11:06:32 +0000

    q4 - qtd vogais

--- lists3(string)/questao5.c
+++ lists3(string)/questao5.c
@@ -3,3 +3,20 @@
 # include <ctype.h>
 
 #define MAX_LENGTH 50 
+
+int qtdConsoantes(char * frase);
+
+int main(){
+    char frase[MAX_LENGTH + 1];
+    printf("Digite uma frase de no máximo 50 caracteres: ");
+    fgets(frase, sizeof(frase), stdin);
+    
+    // Remover o caractere de nova linha adicionado pelo fgets
+    frase[strcspn(frase, "\n")] = '\0';
+    
+    int c = qtdConsoantes(frase);
+    
+    printf("A quantidade de consoantes na frase é: %d\n", c);
+    
+    return 0;
+}
