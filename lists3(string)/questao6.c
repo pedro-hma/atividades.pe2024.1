a1effd7ce263eca7545992e2aea80970a5eda22e
pedro-hma <105680915+pedro-hma@users.noreply.github.com>
2024-04-25 11:26:47 +0000

    questao 6 - contar um caracterer especifico

---
+++ lists3(string)/questao6.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+# include <string.h>
+# include <ctype.h>
+
+#define MAX_LENGTH 100 
+
+int qtdConsoantes(char * frase);
+
+int main(){
+    char frase[MAX_LENGTH + 1];
+    printf("Digite uma frase de no máximo 100 caracteres: ");
+    fgets(frase, sizeof(frase), stdin);
+    
+    // Remover o caractere de nova linha adicionado pelo fgets
+    frase[strcspn(frase, "\n")] = '\0';
+    
+    int c = qtdConsoantes(frase);
+    
+    printf("A quantidade de consoantes na frase é: %d\n", c);
+    
+    return 0;
+}
+int qtdConsoantes(char *frase) {
+    int qtd = 0;
+    for (int i = 0; frase[i] != '\0'; i++) {
+        char c = tolower(frase[i]);
+        if (c == 'a') {// ficar alterando aqui
+            qtd++;
+        }
+    }
+    return qtd;
+}
